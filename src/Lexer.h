bffc1328f40ed4f5ccdeccd0d1de3d228ccd34ae
BluuWasTaken <pjcrowe4@gmail.com>
2021-03-20 16:10:40 +0000

    Added new functions FullLex, FreeTokens

--- src/Lexer.h
+++ src/Lexer.h
@@ -7,3 +7,7 @@ char** Tokenize(const char* Source, int* Size);
 //Tokens is the value returned from Tokenize, Size is the size returned from Tokenize,
 //OutSize is the size of the Token array returned by this function
 Token* Lex(char** Tokens, int Size, int* OutSize);
+
+void FreeTokens(Token* Tokens, int TokenSize);
+
+Token* FullLex(const char* Source, int* TokenSize);
