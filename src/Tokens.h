a5a120657692bf8a8a445551af04922c869d8f68
BluuWasTaken <pjcrowe4@gmail.com>
2021-03-20 16:12:55 +0000

    Added some new tokens, new type token

--- src/Tokens.h
+++ src/Tokens.h
@@ -10,21 +10,25 @@ typedef enum
 	OPEN_CURLY_BRACKET,
 	CLOSE_CURLY_BRACKET,
 	TYPE,
+	COLON,
 	SEMICOLON,
 	EQUAL,
 	UNINITIALIZED = -1,
 	NULL_TOKEN = -2,
 } TokenEnum;
 
+///This matches the definition in Parser.h, except for T_NONE
 typedef enum
 {
 	T_INT,
+	T_FLOAT,
 	T_STRING,
 	T_CHAR,
-	T_FLOAT,
-	T_STRUCTURE,
-	//Used internally for the lexer
-	T_NONE = -1,
+
+	//Other
+	T_STRUCT,
+
+	T_NONE,
 } TypeEnum;
 
 typedef struct
