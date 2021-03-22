e40f4dfb3acf91f7671deef82e60bbe11f294637
BluuWasTaken <pjcrowe4@gmail.com>
2021-03-22 08:26:11 +0000

    Moved most of the stuff in Tokens.h to LexerParserDependencies.h

--- src/Tokens.h
+++ src/Tokens.h
@@ -11,31 +11,9 @@ typedef enum
 	CLOSE_CURLY_BRACKET,
 	TYPE,
 	COLON,
+	COMMA,
 	SEMICOLON,
 	EQUAL,
 	UNINITIALIZED = -1,
 	NULL_TOKEN = -2,
 } TokenEnum;
-
-///This matches the definition in Parser.h, except for T_NONE
-typedef enum
-{
-	T_INT,
-	T_FLOAT,
-	T_STRING,
-	T_CHAR,
-
-	//Other
-	T_STRUCT,
-
-	T_NONE,
-} TypeEnum;
-
-typedef struct
-{
-	TokenEnum Token;
-	TypeEnum Type;
-
-	void* Value;
-	int Size;
-} Token;
