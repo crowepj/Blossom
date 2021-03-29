27d2223613ea9c40aa5e00713fcd09fe624c2565
BluuWasTaken <pjcrowe4@gmail.com>
2021-03-29 21:35:37 +0100

    Updated

--- src/Lexer.h
+++ src/Lexer.h
@@ -3,7 +3,7 @@
 #include "Tokens.h"
 
 //Size is the length of the outputted array, it is set in the function
-char** Tokenize(const char* Source, int* Size);
+char** Tokenize(const char* Source, int* Size, int InLen);
 
 //Tokens is the value returned from Tokenize, Size is the size returned from Tokenize,
 //OutSize is the size of the Token array returned by this function
@@ -11,4 +11,4 @@ Token* Lex(char** Tokens, int Size, int* OutSize);
 
 void FreeTokens(Token* Tokens, int TokenSize);
 
-Token* FullLex(const char* Source, int* TokenSize);
+Token* FullLex(const char* Source, int Len, int* TokenSize);
